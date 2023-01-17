389484b20851dbc2b568a8408062d743de32ba8b
zivbabs <117026503+zivbabs@users.noreply.github.com>
2023-01-17 12:24:08 +0100

    #5 Array [01] uploaded with answers.

---
+++ src/MyArray.java
@@ -0,0 +1,15 @@
+public class MyArray {
+    public static void main(String[] args){
+        int[] a = {4,8,15,16,23,42};
+        int[] b = new int[6];
+        int sum = 0;
+        for(int value: a){
+            sum += value;
+        }
+        System.out.println(sum);
+        b = a;
+        /*
+        When we do b = a, b copies the adresses of stored inside a that points to the adresses of the variables.
+         */
+    }
+}
